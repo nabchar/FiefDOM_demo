a5b1bf2c130d2b030acf2bc80b75335d24887eb3
Nick Char <nick.char.nick@gmail.com>
2017-05-11 17:24:02 -0400

    Add gif to modal

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,20 @@
-export default () => {
+const openModal = () => {
     // Get the modal
   let modal = document.getElementById('myModal');
-  modal.style.display = "block";
+
+  const fetchGiphy = () => {
+    let offset = Math.floor(Math.random() * 100);
+    return $f.ajax({
+      method: 'GET',
+      url:`http://api.giphy.com/v1/gifs/search?q=game+over&api_key=dc6zaTOxFJmzC&limit=1&offset=${offset}`
+    });
+  }
+
+  fetchGiphy().then((res) => {
+    debugger
+    $f('#game-over').attr("src", res.data[0].images.downsized.url)
+    modal.style.display = "block";
+  })
 
   // Get the <span> element that closes the modal
   let span = document.getElementsByClassName("close")[0];
@@ -10,3 +23,5 @@ export default () => {
       modal.style.display = "none";
   }
 };
+
+module.exports = openModal;
